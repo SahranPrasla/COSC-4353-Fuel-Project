9b22839826a714ad04523b553b611204ea420ddc
KabeerAli <kabeerali616@gmail.com>
2023-03-24 00:46:31 -0500

    changed port to 4000

--- profileManagementServer.js
+++ profileManagementServer.js
@@ -14,18 +14,17 @@ app.get('/saveProfile', (req, res) => {
 // Route for handling login requests
 app.post('/saveProfile', (req, res) => {
   const { fullName, address1, address2, city, state, zipcode } = req.body;
-  console.log(fullName)
   if (fullName == 'KabeerAli' && address1 == '786 Haye Dr' && address2 == 'N/A' && city == 'Houston' 
   && state == 'TX' && zipcode == '77564') {
-    res.status(200).send('Profile successful!' + zipcode);
+    res.status(200).send('Profile successful!');
   } else {
     res.status(401).send('Invalid Profile');
   }
 });
 
 // Start the server on port 3000
-const server = app.listen(3000, () => {
-  console.log('Server started on port 3000');
+const server = app.listen(4000, () => {
+  console.log('Server started on port 4000');
 });
 
 module.exports = server;
