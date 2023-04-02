d2780b7c5fc61eefa17312068ecf6eca211a9fb7
Ziyanmaknojia <ziyanmak@gmail.com>
2023-04-01 22:13:45 -0500

    change database.js

--- database.js
+++ database.js
@@ -3,18 +3,4 @@ var pg = require('pg');
 var conString = "postgres://brqwgzyf:1xJT2dMuCrI8dArQGhquZz4RGJjPdR9H@raja.db.elephantsql.com/brqwgzyf"
 var client = new pg.Client(conString);
 
-client.connect(function(err) {
-    if(err) {
-      return console.error('could not connect to postgres', err);
-    }
-    client.query('SELECT * from UserCredentials;', function(err, result) {
-      if(err) {
-        return console.error('error running query', err);
-      }
-      console.log(result.rows);
-        client.end();
-    });
-    
-  });
-
-  module.exports = client;
+module.exports = client;
