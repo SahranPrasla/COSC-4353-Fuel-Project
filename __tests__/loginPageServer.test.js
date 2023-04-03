88dbc56bbd2a62ca9fc1ce8189f98cae2ab87f6c
Ziyanmaknojia <ziyanmak@gmail.com>
2023-04-02 22:51:44 -0500

    log in and signup database all now work well!

--- __tests__/loginPageServer.test.js
+++ __tests__/loginPageServer.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest');
 const app = require('../loginPageServer.js');
 
-describe('POST /GallonQuoteForm.html', () => {
+describe('POST /login', () => {
   it('returns 200 OK and "Login successful!" message when given correct credentials', async () => {
     const res = await request(app)
-    .post('/GallonQuoteForm.html')
-    .send({ username: 'myusername', password: 'mypassword' });
+    .post('/login')
+    .send({ username: 'username', password: 'password' });
     expect(res.statusCode).toBe(200);
     expect(typeof(res.text)).toBe("string");
     expect(res.text).toMatch('Login successful!');
@@ -13,7 +13,7 @@ describe('POST /GallonQuoteForm.html', () => {
 
   it('returns 401 Unauthorized when given incorrect credentials', async () => {
     const res = await request(app)
-    .post('/GallonQuoteForm.html')
+    .post('/login')
     .send({ username: 'invalidusername', password: 'invalidpassword' });
     expect(res.statusCode).toBe(401);
     expect(typeof(res.text)).toBe("string");
--- loginPageServer.js
+++ loginPageServer.js
@@ -1,19 +1,47 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
+const path = require('path')
+const client = require('./database.js');
 
-// Use middleware to parse incoming request bodies
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // Route for handling login requests
-app.post('/GallonQuoteForm.html', (req, res) => {
+app.route('/login')
+.get((req,res) => {
+  res.sendFile(path.join(__dirname+'/public/loginPage.html'));
+})
+.post(async(req, res) => {
   const { username, password } = req.body;
-  if (username === 'myusername' && password === 'mypassword') {
-    res.status(200).send('Login successful!');
-  } else {
-    res.status(401).send('Invalid username or password');
-  }
+  
+  client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    client.query("SELECT * FROM UserCredentials WHERE username ='"+username+"';", function(err, result) {
+      if(result.rowCount == 0) {
+        res.sendFile(path.join(__dirname+'/public/loginPage.html'));
+       console.log("Invalid username or password");
+      }
+      else if (username == result.rows[0].username && password == result.rows[0].password) {
+        res.status(200).sendFile(path.join(__dirname+'/public/GallonQuoteForm.html'));
+      }
+      else if(err) {
+        return console.error('error running query', err);
+      }
+      else {
+        res.status(401).sendFile(path.join(__dirname+'/public/loginPage.html'));
+      }
+      client.end();
+    });
+  });
+
+
+
+  // if (username == usr1 && password == pwd1) {
+  //   res.status(200).sendFile(path.join(__dirname+'/public/GallonQuoteForm.html'));
+  // } 
 });
 
 // Start the server on port 3000
