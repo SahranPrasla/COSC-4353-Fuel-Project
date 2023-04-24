cf48bb1e43bc7bef948a6316f564adc6f4b2a71d
Sahran Prasla <sahranprasla@Sahrans-MacBook-Pro.local>
2023-04-23 21:24:27 -0500

    Added Price class and added new button to gallon quote form.

---
+++ PriceModule.js
@@ -0,0 +1,27 @@
+class Price {
+  #current_ppg = 1.50;
+  #company_profit = 0.10;
+  #location_factor;
+  #history_factor;
+  #gallons_req_factor;
+  #gallons_requested;
+
+  constructor(location_factor, history_factor, gallons_req_factor, gallons_requested){
+    this.#location_factor = location_factor;
+    this.#history_factor = history_factor;
+    this.#gallons_req_factor = gallons_req_factor;
+    this.#gallons_requested = gallons_requested;
+  }
+
+  getMargin() {
+    return this.#current_ppg * (this.#location_factor - this.#history_factor + this.#gallons_req_factor + this.#company_profit);
+  }
+
+  getSuggestedPPG(){
+    return this.getMargin() * this.#current_ppg;
+  }
+
+  getTotal(){
+    return this.getSuggestedPPG() * this.#gallons_requested;
+  }
+}
